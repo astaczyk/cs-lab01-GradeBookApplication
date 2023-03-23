30167825c5dc2b7217f10b44ee9c912d102aad70
Adrian Stanczyk <stanczyk.adrian@gmail.com>
2023-03-23 13:28:15 +0100

    CreateStandardGradeBookAndRankedGradeBookClass

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+            
+        }
+    }
+}
